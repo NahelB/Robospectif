b482a267765fe9ace00aaae5906c48bd22c6aa97
lucachapelle <36231726+lucachapelle@users.noreply.github.com>
2019-02-01 17:02:35 +0100

    ajout du code de la table dans projet
    ajout du code de la table dans projet
    luca

---
+++ projet/table/Assets/Script/ScriptU/ScriptsSC2/sc.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class sc : MonoBehaviour
+{
+    public GameObject buttonJ1;
+    public GameObject buttonJ2;
+    public GameObject buttonJ3;
+    public GameObject buttonJ4;
+    public GameObject buttonJ5;
+    public GameObject buttonJ6;
+    public GameObject buttonplay;
+
+
+
+    //public b1 b1;
+
+    public static Vector2 _position1 = new Vector2(960+130,540+-410);
+    public static Vector2 _position2 = new Vector2(960+-870,540+-410);
+    public static Vector2 _position3 = new Vector2(960+-560,540+100);
+    public static Vector2 _position4 = new Vector2(960+-870,540+410);
+    public static Vector2 _position5 = new Vector2(960+130,540+410);
+    public static Vector2 _position6= new Vector2(960+560,540+100);
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonJ1.transform.position = new Vector2(960+450,540+-350);
+        buttonJ2.transform.position = new Vector2(960+-450,540+-350);
+        buttonJ3.transform.position = new Vector2(960+-750,540+0);
+        buttonJ4.transform.position = new Vector2(960+-450,540+350);
+        buttonJ5.transform.position = new Vector2(960+450,540+350);
+        buttonJ6.transform.position = new Vector2(960+750,540+0);
+        buttonplay.SetActive(false);
+        text.text = "il y a " + Main.Global.Nbjoueur + " joueurs sélectionnés ";
+        text.fontSize = 25;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Main.Global.Nbjoueur = b1.estselec + b2.estselec + b3.estselec + b4.estselec + b5.estselec + b6.estselec;
+        
+        text.text = "il y a " + Main.Global.Nbjoueur + " joueurs sélectionnés ";
+        if (Main.Global.Nbjoueur >= 4)
+        {
+            buttonplay.SetActive(true);
+
+        }
+        else
+        {
+            buttonplay.SetActive(false);
+
+        }
+
+    }
+   
+}
---
+++ projet/table/Assets/Script/user2image2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class user2image2 : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    private Sprite pics;
+    public Image image;
+
+    private Main.TabImage sprite;
+
+    private Sprite[] tab;
+    // Start is called before the first frame update
+    void Start()
+    {         
+        sprite = Main.Global.TabD;
+        tab = new Sprite[2];
+        int x;
+        int y;
+        x = Random.Range(1,sprite.getTaille());
+        y = Random.Range(1,sprite.getTaille());
+        while (x == y)
+        {
+            y = Random.Range(1,sprite.getTaille());
+             
+        }
+
+        tab[0] = sprite.getImageind(x).getSprite();
+        tab[1] = sprite.getImageind(y).getSprite();
+
+        pics = tab[0];
+        ShowRandomImage();
+      
+    }
+
+    void ShowRandomImage()
+    {
+        
+        image.sprite = pics;
+        image.enabled = true;   
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (pics == tab[1])
+        {
+            pics = tab[0];
+
+        }
+        else
+        {
+            pics = tab[1];
+
+        }
+        ShowRandomImage();
+
+    }
+}
